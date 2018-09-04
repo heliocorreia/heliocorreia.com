64e66ab00f49d72d40a48f4d54fe9de6a2620748
Hélio Correia <helio@corp.globo.com>
2018-09-03 22:53:04 -0300

    add favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,12 @@
-module.exports = {
-  dir: {
-      input: "src/structure",
-      output: "docs"
-  },
-  templateFormats: ["njk", "html", "md"]
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/structure/favicon.ico");
+
+  return {
+    dir: {
+        input: "src/structure",
+        output: "docs"
+    },
+    passthroughFileCopy: true,
+    templateFormats: ["njk", "html", "md"]
+  }
 };
