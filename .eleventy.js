b1f6142b2b9c86cf3c4dd9d019e21ac08fc615ae
Hélio Correia <helio@corp.globo.com>
2018-09-22 23:02:46 -0300

    update favicons

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/structure/img");
   eleventyConfig.addPassthroughCopy("src/structure/favicon.ico");
   eleventyConfig.addPassthroughCopy("src/structure/andrew-neel-308138-unsplash-low.jpg");
 
